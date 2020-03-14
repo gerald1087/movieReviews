5c0209d5a1b5fae5b89605515872b618413f95d0
Gerald Whitehead <geraldw@Geralds-MacBook-Air.local>
2020-03-14 14:00:18 -0400

    working movie render component

---
+++ frontend/movie-reviews/src/MovieSearch.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import SearchResults from './SearchResults'
+
+
+export default class MovieSearch extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            movie: "",
+            results: [],
+        }
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        let searchString = this.state.movie
+        let urlEncodedSearchString = encodeURIComponent(searchString);
+        axios.get("https://www.omdbapi.com/?apikey=fe66f057&t=" + urlEncodedSearchString)
+            .then(response => {
+                console.log(response);
+                this.setState({
+                    results: response.data
+                })
+                console.log(this.state.results)
+
+            }).catch(error => {
+                console.log(error);
+            });
+    };
+
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value,
+        })
+    }
+    render() {
+        // const {results} = this.state
+        return (
+            <div>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="movieSearch">Movie Search</label>
+                        <input type="text" name="movie" className="form-control" id="movieSearchBar" value={this.state.movie} onChange={this.handleChange} />
+                    </div>
+                    <button type="submit" className="btn btn-primary" onClick={(event) => this.handleSubmit(event)} >Submit</button>
+                </form>
+                { this.state.results && 
+                    <SearchResults results={this.state.results} title={this.state.results.Title} image={this.state.results.Poster} year={this.state.results.Year} genre={this.state.results.Genre} plot={this.state.results.Plot}/>
+                    }
+{/* {results.map(result => (
+    <SearchResults title={result.Title} image={result.Poster} year={result.Year}/>
+))} */}
+
+                </div>
+                )
+            }
+}
+
---
+++ frontend/movie-reviews/src/SearchResults.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+
+export default class SearchResults extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            results: this.props.results
+        }
+    }
+    render() {
+
+        return (
+            <div>
+                <div>
+                    <div className="card" style={{width: '18rem'}}>
+                        <img src={this.props.image} className="card-img-top" alt="could not load" />
+                            <div className="card-body">
+                                <h5 className="card-title">{this.props.title}</h5>
+        <p className="card-text">{this.props.year}</p>
+        <p className="card-text">{this.props.genre}</p>
+        <p className="card-text">{this.props.plot}</p>
+                                {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+                            </div>
+</div>
+                    </div>
+            </div>
+        )
+    }
+}
